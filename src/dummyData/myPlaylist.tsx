9c9ef8dd504cfb81f461413c0c75d9cc09ae192e
vishant007 <vishant9797@gmail.com>
2024-04-21 20:58:05 +0530

    fix: img format

--- src/dummyData/myPlaylist.tsx
+++ src/dummyData/myPlaylist.tsx
@@ -4,25 +4,25 @@ export const MyPlaylist: CardProps[] = [
 	{
 		title: "Today's Hot Hits",
 		subtitle: 'Various Artists',
-		image: '/_assets/img1.jpg',
+		image: '/_assets/img1.webp',
 		time: '1hr45min',
 	},
 	{
 		title: 'Bhayanak Atma',
 		subtitle: 'Nucleya',
-		image: '/_assets/img2.jpg',
+		image: '/_assets/img2.webp',
 		time: '13hr14 min',
 	},
 	{
 		title: 'Jadi Buti',
 		subtitle: 'Nucleya',
-		image: '/_assets/img4.jpg',
+		image: '/_assets/img4.webp',
 		time: '55min',
 	},
 	{
 		title: 'Lights',
 		subtitle: 'Nucleya',
-		image: '/_assets/img5.jpg',
+		image: '/_assets/img5.webp',
 		time: '6hr',
 	},
 ];
--- src/dummyData/recentlyPlayed.tsx
+++ src/dummyData/recentlyPlayed.tsx
@@ -4,25 +4,25 @@ export const recentlyPlayed: CardProps[] = [
 	{
 		title: "Today's Hot Hits",
 		subtitle: 'Various Artists',
-		image: '/_assets/img1.jpg',
+		image: '/_assets/img1.webp',
 		time: '4 min ago',
 	},
 	{
 		title: 'Bhayanak Atma',
 		subtitle: 'Nucleya',
-		image: '/_assets/img2.jpg',
+		image: '/_assets/img2.webp',
 		time: '1 hour ago',
 	},
 	{
 		title: 'Jadi Buti',
 		subtitle: 'Nucleya',
-		image: '/_assets/img4.jpg',
+		image: '/_assets/img4.webp',
 		time: '5 hours ago',
 	},
 	{
 		title: 'Lights',
 		subtitle: 'Nucleya',
-		image: '/_assets/img5.jpg',
+		image: '/_assets/img5.webp',
 		time: 'a day ago',
 	},
 ];
